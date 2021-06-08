3c9a5ebcc4172dcea168e702b6092fd4461be37a
Hui Yu <ityuhui@gmail.com>
2021-06-08 22:30:26 +0800

    0608-1

--- main.go
+++ main.go
@@ -4,60 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	_ "time"
-
-	"github.com/gorilla/websocket"
 )
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-)
-
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	var (
-		wbsCon *websocket.Conn
-		err    error
-		data   []byte
-	)
-
-	if wbsCon, err = upgrader.Upgrade(w, r, nil); err != nil {
-		return
-	}
-
-	/*go func() {
-		var (
-			err error
-		)
-		for {
-			if err = wbsCon.WriteMessage(websocket.TextMessage, []byte("Websocket heartbeat from webserver-go")); err != nil {
-				return
-			}
-			time.Sleep(10 * time.Second)
-		}
-	}()*/
-
-	for {
-		if _, data, err = wbsCon.ReadMessage(); err != nil {
-			goto ERR
-		}
-		fmt.Println("Received:")
-		fmt.Println(data)
-		if err = wbsCon.WriteMessage(websocket.TextMessage, data); err != nil {
-			goto ERR
-		}
-		fmt.Println("Sent:")
-		fmt.Println(data)
-	}
-
-ERR:
-	wbsCon.Close()
-
-}
 
 // AllowCrossDomain : Add http response header to allow cross domain
 func AllowCrossDomain(w http.ResponseWriter) {
@@ -78,14 +25,8 @@ func main() {
 	fmt.Println("Webserver-go starts...")
 	fmt.Println(" * static file serves at http://0.0.0.0:8080/")
 	fmt.Println(" * RESTful API serves at http://0.0.0.0:8080/api/")
-	fmt.Println(" * websocket serves at 0.0.0.0:8080/ws")
 	http.Handle("/", http.FileServer(http.Dir("public/")))
 	http.HandleFunc("/api/welcome", welcomeHandler)
-
-	InitCheckInApp()
-	http.HandleFunc("/api/checkindata", CheckInDataHandler)
-
-	http.HandleFunc("/ws", wsHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
